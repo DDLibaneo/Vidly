fdfd899036e9c1c53c591b662da5ce7b7f990928
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2019-07-10 16:27:40 -0300

    Separa com espaços as propriedades da classe Model MembershipType

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,25 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class MembershipType
     {        
         public byte Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public short SignUpFee { get; set; }
+
         public byte DurationInMonths { get; set; }
+
         public byte DiscountRate { get; set; }
 
+        // Ids dos MembershipTypes no Banco
         public static readonly byte Unknown = 0;
+
         public static readonly byte PayAsYouGo = 1;
+
         public static readonly byte Monthly = 2;
+
         public static readonly byte Quarterly = 3;
+
         public static readonly byte Yearly = 4;
     }
 }
