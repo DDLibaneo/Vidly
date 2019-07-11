b7fb7b1659138ae4439513ebcc68d5e24e9e75f3
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2019-07-11 18:46:45 -0300

    Add Filter Authorize globalmente

--- Vidly/App_Start/FilterConfig.cs
+++ Vidly/App_Start/FilterConfig.cs
@@ -7,7 +7,8 @@ namespace Vidly
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute()); // Este filter redireciona o usuário para uma página de erro quando uma Action retorna uma mensagem de erro.
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
