2aba04fdd08506b9fc39a41f66991928481ea4d9
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2019-07-19 16:05:58 -0300

    Muda o nome do arquivo IdentityModels para ApplicationDbContext

---
+++ Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Vidly.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //putting a property here makes DbContext aware of the Customer class
+        public DbSet<Customer> Customers { get; set; } //This DbSet represents our customer table in our database
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false) { }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
